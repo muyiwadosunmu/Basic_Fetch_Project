b2f351d31425d7aa34f050e3f3c7641dd45df0b2
Oluwamuyiwa Dosunmu <oluwamuyiwadosunmu@gmail.com>
2023-01-11 17:26:28 +0100

    First Push

---
+++ src/Form.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Form = ({reqType, setReqType}) => {
+  return (
+    <form action="" onSubmit={(e) => e.preventDefault}>
+
+    </form>
+  )
+}
+
+export default Form
